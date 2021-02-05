c98f267a2036c2879365edcc6e30148276584616
meitung473 <48000299+meitung473@users.noreply.github.com>
2021-02-05 14:22:48 +0800

    Add files via upload

---
+++ js/customJS.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // gototop
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+    $('#toTopBtn').fadeIn();
+    $('.navbar').addClass("shrink");
+    } else {
+    $('#toTopBtn').fadeOut();
+    $('.navbar').removeClass("shrink");
+    }
+    });
+    
+    $('#toTopBtn').click(function() {
+    $("html, body").animate({
+    scrollTop: 0
+    }, 1000);
+    return false;
+    });
+});
