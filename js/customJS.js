b46c8543d442ef8058cb96b153e29254d3b23901
Yan Shan Hong <a8870506@gmail.com>
2021-03-01 10:40:41 +0800

    654u4u4u
    hrdhrhr

--- js/customJS.js
+++ js/customJS.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-    
+    $('#toTopBtn').fadeOut();
     // gototop
     $(window).scroll(function() {
     if ($(this).scrollTop() > 20) {
     $('#toTopBtn').fadeIn();
     $('.navbar').addClass("shrink");
-    } else {
+    }
+    else {
     $('#toTopBtn').fadeOut();
     $('.navbar').removeClass("shrink");
     }
